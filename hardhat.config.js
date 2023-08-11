cf41b8501df868051d10582e38d0b224740b0377
Saurabh Lodha <saurabhlodha221b@gmail.com>
2023-08-12 02:40:42 +0530

    Added required networks and setup hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,32 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.19",
+  solidity: {
+    version: "0.8.18",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  networks: {
+    localhost: {
+      url: "http://localhost:8545",
+    },
+    polygonMumbai: {
+      url: process.env.POLYGON_MUMBAI_RPC_URL,
+      accounts: [`0x${process.env.PRIVATE_KEY}`],
+    },
+    optimismGoerli: {
+      url: process.env.OPTIMISM_GOERLI_RPC_URL,
+      accounts: [`0x${process.env.PRIVATE_KEY}`],
+    },
+    baseGoerli: {
+      url: process.env.BASE_GOERLI_RPC_URL,
+      accounts: [`0x${process.env.PRIVATE_KEY}`],
+    },
+  },
 };
